b22d2c6f53988c930fe7b7639af1ff56582f1f96
shathaosa <shatha.osa10@gmail.com>
2025-06-30 09:42:35 +0300

    app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,30 +14,4 @@ function App() {
   );
 }
 
-// Separate component for home content
-function HomeContent() {
-  return (
-    <>
-    <HomeNavbar />
-      <section className="hero">
-        <img src={hero} alt="hero" />
-        <h1>إِنَّ اللَّهَ يُحِبُّ الَّذينَ يُقاتِلونَ فى سَبيلِهِ صَفًّا كَأَنَّهُم بُنيانٌ مَرصوصٌ</h1>
-        <h5>[الصف: 4 - 4]</h5>
-      </section>
-      <section className="about">
-        <h3>
-          هنا تُروى الغزوات والفتوحات الإسلامية، لا كصفحات في كتاب... بل كتجربة تفاعلية حيّة تحكي القصة، تكشف الأسباب، وتعرض النتائج. تعلّم، استكشف، وتأمّل... كيف صُنعت لحظات غيرت مجرى التاريخ.
-        </h3>
-        <img src={camels} alt="الجمال" />
-      </section>
-      <section className="timeline">
-        <h2>الخط الزمني</h2>
-      </section>
-      <section className="map">
-        <h2>الخريطة التفاعلية</h2>
-      </section>
-    </>
-  );
-}
-
 export default App;
