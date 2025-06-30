b3d22a1c252720251002461fe9aedeb64e883fcd
Joudj7tam <joodtalbz@gmail.com>
2025-06-30 17:28:13 +0300

    story page 2

--- frontend/src/Components/storyHeaderBanner.jsx
+++ frontend/src/Components/storyHeaderBanner.jsx
@@ -5,15 +5,17 @@ import '../CSS/storyHeaderBanner.css'
 const HeaderBanner = ({ title, navigationTitle, showPlayButton, onPlayClick }) =>{
   return (
     <div className="header-banner">
-      
-      <h1 className="banner-title">{title}</h1>
-      <h3 className="navigation-title">{navigationTitle}</h3>
 
+      <h1 className="banner-title">{title}</h1>
+      
       {showPlayButton && (
         <button className="play-button" onClick={onPlayClick}>
           <img src={playButton} alt="Play" className="play-icon" />
         </button>
       )}
+
+      <a href="#" className="navigation-title" >{navigationTitle}</a>
+
     </div>
   );
 };
