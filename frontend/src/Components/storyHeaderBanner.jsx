88fd7211d537db32265f659d943a05e20dd1a716
Joudj7tam <joodtalbz@gmail.com>
2025-07-03 20:51:25 +0300

    story page style2

--- frontend/src/Components/storyHeaderBanner.jsx
+++ frontend/src/Components/storyHeaderBanner.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import playButton from '../assets/playButton.png';
+//import islamic_war_video from '../assets/islamic-war.mp4'
 import '../CSS/storyHeaderBanner.css';
 
 const HeaderBanner = ({ title, showPlayButton, onPlayClick, activeTab, setActiveTab }) => {
   return (
     <div className="header-banner">
+      {/*
+      <video 
+        className="header-video" 
+        autoPlay 
+        muted 
+        loop 
+        playsInline
+      >
+        <source src={islamic_war_video} type="video/mp4" />
+        Your browser does not support HTML5 video.
+      </video> 
+         */}
       <div className="segmented-control" data-active={activeTab}>
         <button
           className={`segment ${activeTab === 'story' ? 'active' : ''}`}
