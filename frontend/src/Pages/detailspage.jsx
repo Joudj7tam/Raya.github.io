ab4dc7524e3f1e38eee8db0fc04b1b5e5f088d62
Joudj7tam <joodtalbz@gmail.com>
2025-07-03 13:28:03 +0300

    details page

--- frontend/src/Pages/detailspage.jsx
+++ frontend/src/Pages/detailspage.jsx
@@ -15,7 +15,7 @@ import '../CSS/detailspage.css';
 const details = () => {
     return (
         <div className='body'>
-            <Navbar/>
+            <Navbar />
             <StoryHeaderBanner
                 title="غزوة بدر"
                 showPlayButton={false}
@@ -23,54 +23,52 @@ const details = () => {
             />
             <div className='page-content-container'>
                 <DetailsBox dir="rtl" lang="ar"
-                icon= {sword_icon}
-                title= "نوع الغزوة"
-                text= "هجومية"
+                    icon={sword_icon}
+                    title="نوع الغزوة"
+                    text="هجومية"
                 />
                 <DetailsBox dir="rtl" lang="ar"
-                icon= {calender_icon}
-                title= "التاريخ"
-                text= "2هـ 17 رمضان "
+                    icon={calender_icon}
+                    title="التاريخ"
+                    text="2هـ 17 رمضان "
                 />
                 <DetailsBox dir="rtl" lang="ar"
-                icon= {muslim_icon}
-                title= "قائد السلمين"
-                text= "النبي محمد ﷺ"
+                    icon={muslim_icon}
+                    title="قائد السلمين"
+                    text="النبي محمد ﷺ"
                 />
                 <DetailsBox dir="rtl" lang="ar"
-                icon= {army_icon}
-                title= "عدد المسلمين"
-                text= "نحو 313"
+                    icon={army_icon}
+                    title="عدد المسلمين"
+                    text="نحو 313"
                 />
                 <DetailsBox dir="rtl" lang="ar"
-                icon= {enemy_icon}
-                title= "قائد المشركين"
-                text= "أبو جهل بن هشام"
+                    icon={enemy_icon}
+                    title="قائد المشركين"
+                    text="أبو جهل بن هشام"
                 />
                 <DetailsBox dir="rtl" lang="ar"
-                icon= {army_icon}
-                title= "عدد المشركين"
-                text= "من 900 إلى 1000"
+                    icon={army_icon}
+                    title="عدد المشركين"
+                    text="من 900 إلى 1000"
                 />
                 <DetailsBox dir="rtl" lang="ar"
-                icon= {flag_icon}
-                title= "النتيجة"
-                text= "انتصار المسلمين"
+                    icon={flag_icon}
+                    title="النتيجة"
+                    text="انتصار المسلمين"
                 />
                 <DetailsBox dir="rtl" lang="ar"
-                icon= {question_icon}
-                title= "أسبابها"
-                text= " بدأت المعركة بمحاولة المسلمين القضاء على الخطر المحدق المتمثل باعتراضَ عيرٍ لقريشٍ متوجهةٍ من الشام إلى مكة يقودها أبو سفيان بن حرب، ولكن أبا سفيان تمكن من الفرار بالقافلة، وأرسل رسولاً إلى قريش في مكة يطلب عونهم ونجدتهم. "
+                    icon={question_icon}
+                    title="أسبابها"
+                    text=" بدأت المعركة بمحاولة المسلمين القضاء على الخطر المحدق المتمثل باعتراضَ عيرٍ لقريشٍ متوجهةٍ من الشام إلى مكة يقودها أبو سفيان بن حرب، ولكن أبا سفيان تمكن من الفرار بالقافلة، وأرسل رسولاً إلى قريش في مكة يطلب عونهم ونجدتهم. "
                 />
                 <DetailsBox dir="rtl" lang="ar"
-                icon= {mountain_icon}
-                title= "آثارها"
-                text= " بداية مرحلة جديدة في تاريخ الإسلام, زيادة العداوة بين المسلمين و قريش, أصبح للمسلمين مصدرٌ جديدٌ للدخل وهو غنائم المعارك، وبذلك تحسّن حالُ المسلمين الماديّ والاقتصاديّ والمعنويّ." 
+                    icon={mountain_icon}
+                    title="آثارها"
+                    text=" بداية مرحلة جديدة في تاريخ الإسلام, زيادة العداوة بين المسلمين و قريش, أصبح للمسلمين مصدرٌ جديدٌ للدخل وهو غنائم المعارك، وبذلك تحسّن حالُ المسلمين الماديّ والاقتصاديّ والمعنويّ."
                 />
-
-
             </div>
-            
+
         </div>
     );
 }
