db4c3a492ab4bc8f70f33ba6016ef1540ab5637f
Joudj7tam <joodtalbz@gmail.com>
2025-07-06 11:21:23 +0300

    website styles

--- frontend/src/Pages/Homepage.jsx
+++ frontend/src/Pages/Homepage.jsx
@@ -1,14 +1,22 @@
+import React, { useState, useEffect } from 'react';
 import camels from "../assets/camels.png";
 import HomeNavbar from "../Components/homeNavbar";
 import Timeline from "../Components/timeline";
 import "../CSS/home.css";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 function HomePage() {
+
+  useEffect(() => {
+    AOS.init({ duration: 1000 });
+  }, []);
+
   return (
     <>
       <HomeNavbar />
       <section className="hero">
-        <div className="hero-text">
+        <div className="hero-text" data-aos="fade-right">
           <h1 dir="rtl" lang="ar">﴿ إِنَّ اللَّهَ يُحِبُّ الَّذينَ يُقاتِلونَ فى سَبيلِهِ صَفًّا كَأَنَّهُم بُنيانٌ مَرصوصٌ ﴾</h1>
           <h5>[الصف: 4 - 4]</h5>
         </div>
@@ -16,13 +24,13 @@ function HomePage() {
 
       <div className="flex-container">
         <section className="about">
-          <h3 className="about-text" dir="rtl" lang="ar">
+          <h3 className="about-text" data-aos="fade-up" dir="rtl" lang="ar">
             هنا تُروى الغزوات والفتوحات الإسلامية، لا كصفحات في كتاب... بل كتجربة تفاعلية حيّة تحكي القصة، تكشف الأسباب، وتعرض النتائج. تعلّم، استكشف، وتأمّل... كيف صُنعت لحظات غيرت مجرى التاريخ.
           </h3>
           <img src={camels} alt="الجمال" />
         </section>
-        <section className="timeline">
-          <h2>الخط الزمني</h2>
+        <section className="timeline" data-aos="fade-up">
+          <h2 data-aos="fade-up">استعرض أبرز الغزوات والفتوحات، حسب ترتيبها الزمني</h2>
           <Timeline />
         </section>
         <section className="map">
