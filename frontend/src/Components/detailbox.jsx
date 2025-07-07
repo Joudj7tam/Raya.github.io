6d9ac68ebd02a593536bc1205aa768dd899bf80d
Joudj7tam <joodtalbz@gmail.com>
2025-07-07 11:02:48 +0300

    details page

--- frontend/src/Components/detailbox.jsx
+++ frontend/src/Components/detailbox.jsx
@@ -3,14 +3,14 @@ import AOS from 'aos';
 import 'aos/dist/aos.css';
 import "../CSS/detailbox.css";
 
-const DetailsBox = ({ icon, title, text}) => {
+const DetailsBox = ({ icon, title, text, className}) => {
 
     useEffect(() => {
         AOS.init({ duration: 1000 });
     }, []);
 
     return (
-        <div className="detail-card" data-aos="fade-up">
+        <div className={`detail-card ${className || ''}`} data-aos="fade-up">
             <img src={icon} alt="detail-icon"/>
             <div className="text-box"> 
                 <h3 dir="rtl" lang="ar" >{title}</h3>
--- frontend/src/Components/storyHeaderBanner.jsx
+++ frontend/src/Components/storyHeaderBanner.jsx
@@ -6,7 +6,7 @@ import '../CSS/storyHeaderBanner.css';
 const HeaderBanner = ({ title, showPlayButton, onPlayClick, activeTab, setActiveTab }) => {
   return (
     <div className="header-banner">
-      {
+      {/*
       <video 
         className="header-video" 
         autoPlay 
@@ -17,7 +17,7 @@ const HeaderBanner = ({ title, showPlayButton, onPlayClick, activeTab, setActive
         <source src={islamic_war_video} type="video/mp4" />
         Your browser does not support HTML5 video.
       </video> 
-         }
+         */}
       <div className="segmented-control" data-active={activeTab}>
         <button
           className={`segment ${activeTab === 'story' ? 'active' : ''}`}
