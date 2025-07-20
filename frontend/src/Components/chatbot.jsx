2b5440972824379fd7776afcc7c69e05eb7a4699
EbtehalAlzahrani <Ebtehal.alzah@gmail.com>
2025-07-20 11:58:12 +0300

    chatbot design only and solve scroll proplem in navbar

--- frontend/src/Components/chatbot.jsx
+++ frontend/src/Components/chatbot.jsx
@@ -1,12 +1,63 @@
+import { useEffect, useRef, useState } from "react";
+import ChatbotIcon from "./ChatbotIcon";
+import ChatForm from "./ChatForm";
+import ChatMessage from "./ChatMessage";
 import "../CSS/chatbot.css";
-import eventImage from '../assets/bot.png';
 
 const Chatbot = () => {
+    const chatBodyRef = useRef();
+    const [showChatbot, setShowChatbot] = useState(false);
+    const [chatHistory, setChatHistory] = useState([
+        {
+            role: "model",
+            text: "مرحبًا! كيف يمكنني مساعدتك اليوم؟",
+        },
+    ]);
+
+    useEffect(() => {
+        chatBodyRef.current?.scrollTo({
+            top: chatBodyRef.current.scrollHeight,
+            behavior: "smooth",
+        });
+    }, [chatHistory]);
+
     return (
-        <div className="chatbot-container"> 
-            <button className="chat-button">
-                <img src={eventImage} alt="Chatbot" className="chat-image" />
+        <div className={`container ${showChatbot ? "show-chatbot" : ""}`}>
+            <button
+                onClick={() => setShowChatbot((prev) => !prev)}
+                id="chatbot-toggler"
+            >
+                <span className="material-symbols-rounded">mode_comment</span>
+                <span className="material-symbols-rounded">close</span>
             </button>
+
+            <div className="chatbot-popup">
+                <div className="chat-header">
+                    <div className="header-info">
+                        <ChatbotIcon />
+                        <h2 className="logo-text">Chatbot</h2>
+                    </div>
+                    <button
+                        onClick={() => setShowChatbot((prev) => !prev)}
+                        className="material-symbols-rounded"
+                    >
+                        keyboard_arrow_down
+                    </button>
+                </div>
+
+                <div ref={chatBodyRef} className="chat-body">
+                    {chatHistory.map((chat, index) => (
+                        <ChatMessage key={index} chat={chat} />
+                    ))}
+                </div>
+
+                <div className="chat-footer">
+                    <ChatForm
+                        chatHistory={chatHistory}
+                        setChatHistory={setChatHistory}
+                    />
+                </div>
+            </div>
         </div>
     );
 };
